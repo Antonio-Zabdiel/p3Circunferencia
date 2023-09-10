d27c7f95e790682821178dd773d7c43850c85fb7
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2023-09-09 20:40:38 -0600

    a little bit stressfull with stupid error between decimal and float

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
+/*
+programa que calcula la superficie y la circunferencia de un circulo
+*/
 #include<stdio.h>
 
 int main(){
+    float pi = 3.141592;
+    float diam = 0;
+    float rad = 0;
+    float circ = 0;
+    float super = 0;
+    float cons = 2.00000;
+
+    printf("Hola usuario por favor ingresa el radio de tu circulo: \n");
+    scanf("%f", &rad);
+
+    diam = cons*rad;
+    circ = pi*diam;
+    super = pi*rad*rad;
+
+    printf("==========================================================================\n");
+    printf("el diametro de tu circulo es: %f \n",diam);
+    printf("la circunferencia del circulo es: %f \n",circ);
+    printf("la superficie del circulo es: %f \n",super);
+    printf("==========================================================================\n");
+
     return 0;
 }
